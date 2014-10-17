f85b92b337423a0c8678afd810c90637b41f0b89
Benedikt Valur Arnason <benni@i-bb14f38e.greenqloud.internal>
2014-10-17 18:40:18 +0000

    added another test to test multiple numbers

--- src/test/java/is/ru/stringcalculator/CalculatorTest.java
+++ src/test/java/is/ru/stringcalculator/CalculatorTest.java
@@ -29,4 +29,9 @@ public class CalculatorTest {
     	assertEquals(6, Calculator.add("1,2,3"));
     }
 
+	@Test
+	public void testMultipleNumbers2(){
+	assertEquals(15, Calculator.add("1,2,3,4,5"));
+	}
+
 }
